f368603a59d5a8f3b073d09eef43b59f46db7871
joonaskinnunen <joonas.kinn@gmail.com>
2021-11-14 17:37:19 +0200

    Add Firebase connection

--- MyTattooArtist/app/src/main/java/com/jk/mytattooartist/MainActivity.java
+++ MyTattooArtist/app/src/main/java/com/jk/mytattooartist/MainActivity.java
@@ -1,8 +1,20 @@
 package com.jk.mytattooartist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        testDB();
+    }
+
+    public void testDB() {
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://mytattooartist-d2298-default-rtdb.europe-west1.firebasedatabase.app/");
+        DatabaseReference myRef = database.getReference();
+        myRef.child("message").setValue("Viesti");
+        
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("VALUE: ", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("ERROR: ", "Failed to read value.", error.toException());
+            }
+        });
     }
 }
