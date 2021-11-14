339a7689271c7a88b75b407cddeffffa78009cd7
joonaskinnunen <joonas.kinn@gmail.com>
2021-11-14 19:38:05 +0200

    Add options menu

--- MyTattooArtist/app/src/main/java/com/jk/mytattooartist/MainActivity.java
+++ MyTattooArtist/app/src/main/java/com/jk/mytattooartist/MainActivity.java
@@ -1,20 +1,18 @@
 package com.jk.mytattooartist;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,14 +23,38 @@ public class MainActivity extends AppCompatActivity {
         testDB();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.myaccount:
+                // TODO accountActivity
+                // startActivity(new Intent(this, accountActivity));
+                return(true);
+            case R.id.logout:
+                // TODO logout function
+                // logOut();
+                return(true);
+        }
+
+        return(super.onOptionsItemSelected(item));
+    }
+
+
     public void testDB() {
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance("https://mytattooartist-d2298-default-rtdb.europe-west1.firebasedatabase.app/");
         DatabaseReference myRef = database.getReference();
         myRef.child("message").setValue("Viesti");
-        
+
         // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
+        myRef.child("message").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
