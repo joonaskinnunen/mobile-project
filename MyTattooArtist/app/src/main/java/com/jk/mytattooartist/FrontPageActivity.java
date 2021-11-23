e5f46286547d8c767ca1896bcf7e5be0793e0acb
Esa Tervonen <t9tees00@students.oamk.fi>
2021-11-23 22:00:57 +0200

    filter menu kötöstely

--- MyTattooArtist/app/src/main/java/com/jk/mytattooartist/FrontPageActivity.java
+++ MyTattooArtist/app/src/main/java/com/jk/mytattooartist/FrontPageActivity.java
@@ -2,11 +2,12 @@ package com.jk.mytattooartist;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 
@@ -14,13 +15,32 @@ import org.json.JSONException;
 
 import java.util.ArrayList;
 
-public class FrontPageActivity extends AppCompatActivity {
+public class FrontPageActivity extends BaseActivity {
 
     RecyclerView recyclerView;
+    FloatingActionButton fab, fab1, fab2, fab3;
+    Boolean isFABOpen = false;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.front_page);
+
+        fab = findViewById(R.id.fab);
+        fab1 = findViewById(R.id.fab1);
+        fab2 = findViewById(R.id.fab2);
+        fab3 = findViewById(R.id.fab3);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isFABOpen){
+                    showFABMenu();
+                }else{
+                    closeFABMenu();
+                }
+            }
+        });
+
 
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -36,7 +56,21 @@ public class FrontPageActivity extends AppCompatActivity {
         }
 
         Log.d("GSON",String.valueOf(jsonArray2.get(0)));
-
         Log.d("extras", String.valueOf(arrayList.get(0)));
+
+    }
+
+    private void showFABMenu(){
+        isFABOpen=true;
+        fab1.animate().translationY(-getResources().getDimension(R.dimen.standard_55));
+        fab2.animate().translationY(-getResources().getDimension(R.dimen.standard_105));
+        fab3.animate().translationY(-getResources().getDimension(R.dimen.standard_155));
+    }
+
+    private void closeFABMenu(){
+        isFABOpen=false;
+        fab1.animate().translationY(0);
+        fab2.animate().translationY(0);
+        fab3.animate().translationY(0);
     }
 }
