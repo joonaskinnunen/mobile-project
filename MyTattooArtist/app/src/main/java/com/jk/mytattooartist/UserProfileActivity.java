796ecc55c9a1b8160868a1fdb8c5f1978e1354c6
VisaSimula <visa.simula@gmail.com>
2021-12-14 22:59:03 +0200

    change password user and artist profile now has button that sends reset password link to users email.

--- MyTattooArtist/app/src/main/java/com/jk/mytattooartist/UserProfileActivity.java
+++ MyTattooArtist/app/src/main/java/com/jk/mytattooartist/UserProfileActivity.java
@@ -2,8 +2,14 @@ package com.jk.mytattooartist;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -36,7 +42,31 @@ public class UserProfileActivity extends BaseActivity {
         }
     }
 
-    //TODO: Change PW
+    public void changePasswordClicked(View view){
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        database.getReference().child("users").child("clients").child(mAuth.getCurrentUser().getUid()).child("email").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    String emailAddres = String.valueOf(task.getResult().getValue());
+                    auth.sendPasswordResetEmail(emailAddres)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Log.d("Email", "Email sent.");
+                                        Toast.makeText(UserProfileActivity.this, "Password reset link has been sent to your email", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+
+        });
+    }
 
 
     public void userInfo() {
